51be3b15b976f82b07521bc2deada6f457a8ee8b
suman-malik-reoo <sumanmalik2907@gmail.com>
2023-09-10 03:26:36 +0530

    Adding all initial files to main project...

---
+++ app/src/main/java/com/parolim/aichatbot/MainActivity.java
@@ -0,0 +1,140 @@
+package com.parolim.aichatbot;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    TextView welcomeTextView;
+    EditText messageEditText;
+    ImageButton sendButton;
+
+    List<Message> messageList;
+    MessageAdapter messageAdapter;
+
+    public static final MediaType JSON
+            = MediaType.get("application/json; charset=utf-8");
+
+    OkHttpClient client = new OkHttpClient();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycler_view);
+        welcomeTextView = findViewById(R.id.welcome_text);
+        messageEditText = findViewById(R.id.message_edit_text);
+        sendButton = findViewById(R.id.send_btn);
+
+        messageList = new ArrayList<>();
+
+        messageAdapter = new MessageAdapter(messageList);
+        recyclerView.setAdapter(messageAdapter);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setStackFromEnd(true);
+        recyclerView.setLayoutManager(llm);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String question = messageEditText.getText().toString().trim();
+
+                if (question.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Please ask something", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    addToChat(question,Message.SEND_BY_ME);
+                    messageEditText.setText("");
+                    callAPI(question);
+                    welcomeTextView.setVisibility(View.GONE);
+                }
+            }
+        });
+    }
+
+    void addToChat(String message, String sendBy){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                messageList.add(new Message(message,sendBy));
+                messageAdapter.notifyDataSetChanged();
+                recyclerView.smoothScrollToPosition(messageAdapter.getItemCount());
+            }
+        });
+    }
+
+    void addResponse(String response){
+        messageList.remove(messageList.size()-1);
+        addToChat(response,Message.SEND_BY_BOT);
+    }
+
+    void callAPI (String question){
+
+        messageList.add(new Message("Typing...",Message.SEND_BY_BOT));
+
+        JSONObject jsonBody = new JSONObject();
+        try {
+            jsonBody.put("model","text-davinci-003");
+            jsonBody.put("prompt",question);
+            jsonBody.put("max_tokens",1000);
+            jsonBody.put("temperature",0);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        RequestBody body = RequestBody.create(jsonBody.toString(),JSON);
+        Request request = new Request.Builder()
+                .url("https://api.openai.com/v1/completions")
+                .header("Authorization","Bearer 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")
+                .post(body)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                addResponse("Failed to Load response due to "+e.getMessage());
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()){
+                    try {
+                        JSONObject jsonObject = new JSONObject(response.body().string());
+                        JSONArray jsonArray = jsonObject.getJSONArray("choices");
+                        String result = jsonArray.getJSONObject(0).getString("text");
+                        addResponse(result.trim());
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                else {
+                    addResponse("Failed to Load response due to "+response.body().toString());
+                }
+            }
+        });
+    }
+}
